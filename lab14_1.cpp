44ed5bc6ab90e1ccfc9929a4694f91f5b6093ce2
Homxy <120322865+Homxy@users.noreply.github.com>
2023-01-13 17:15:59 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,26 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    b[5]=100;
+    b[2]=1;
+    for(int i=0;i<n;i++){
+        b[0]+=a[i]/n;
+        b[1] += pow(a[i],2);
+        b[2] *= a[i];
+        b[3] += 1/a[i];
+        if(b[4]<a[i]){
+            b[4]=a[i];
+        }
+        else if(b[5]>a[i]){
+            b[5]=a[i];
+        }
+        
+    }
+    b[1]=sqrt((b[1]/n)-(b[0]*b[0]));
+    b[2]=pow(b[2],1*1.0/n);
+    b[3]=n/b[3];
+    
+    
+}
